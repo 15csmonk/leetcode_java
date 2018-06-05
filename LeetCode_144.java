813eeed1d0cce5e8905b643a76984238aa328926
15csmonk <15jyli5@stu.edu.cn>
2018-06-05 09:15:25 +0800

    提交文件

---
+++ LeetCode_144.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.*;
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if(root == null)
+            return list;
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;
+    }
+}
---
+++ LeetCode_145.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.*;
+public class Solution {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+         ArrayList<Integer> list = new ArrayList<Integer>();
+        if(root == null)
+            return list;
+        list.addAll(postorderTraversal(root.left));
+        list.addAll(postorderTraversal(root.right));
+        list.add(root.val);
+        return list;
+    }
+}
