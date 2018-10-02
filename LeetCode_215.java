c9ee9dd9a24c19af70218cabf70fb971d4ffce11
15csmonk <15jyli5@stu.edu.cn>
2018-10-02 22:33:46 +0800

    Update LeetCode_215.java

--- LeetCode_215.java
+++ LeetCode_215.java
@@ -1,16 +1,6 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int index = 0;
-        int index1 = 0;
-        for(int i = 0; i < nums.length; i ++){
-           for(int j = i+1; j < nums.length; j ++){
-               if(nums[j] == target - nums[i]){
-                   index = i;
-                   index1 = j;
-               }
-           }
-        }
-        int ret[] = {index, index1};
-        return ret;
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
     }
 }
