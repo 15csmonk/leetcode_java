acc0a7665253f49796265cafe9cfa9fd40d862ba
15csmonk <15jyli5@stu.edu.cn>
2018-11-04 20:35:13 +0800

    Update LeetCode_451.java

--- LeetCode_451.java
+++ LeetCode_451.java
@@ -8,8 +8,8 @@ class Solution {
                 s1.put(c, 1);
             }
         }
-        List<Character>[] bucket = new ArrayList[s.length()+1];    
-        for (char k : s1.keySet()) {
+        List<Character>[] bucket = new ArrayList[s.length() + 1];    
+        for(char k : s1.keySet()){
             int v = s1.get(k);
             if(bucket[v] == null){
                 bucket[v] = new ArrayList<>();
@@ -17,10 +17,10 @@ class Solution {
             bucket[v].add(k);
         }
         StringBuilder s2 = new StringBuilder();
-        for(int i = bucket.length - 1; i > 0; i --) {
-            if(bucket[i] != null) {
-                for(char c : bucket[i]) {
-                    for(int j = 0; j < s1.get(c); j ++) {
+        for(int i = bucket.length - 1; i > 0; i --){
+            if(bucket[i] != null){
+                for(char c : bucket[i]){
+                    for(int j = 0; j < s1.get(c); j ++){
                         s2.append(c);
                     }
                 }
